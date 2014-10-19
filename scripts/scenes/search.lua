3ffcadaeaf8c2dbabe365ed81c679c8bc59fe62c
Nyabenda Mporakumugero <nyabenda@intangible.mx>
2014-10-19 16:02:16 -0500

    busqedas

--- scripts/scenes/search.lua
+++ scripts/scenes/search.lua
@@ -13,7 +13,11 @@ local starButton
 local closeButton
 local downloadButton
 local canTouch = true
+local canMove = true
 local up,down
+local offset = _W *0.2
+local group = display.newGroup()
+
 
 ------------Functions.-----------------------------------------------------------------------
 
@@ -25,6 +29,25 @@ local function close(event)
         composer.hideOverlay( "fade", 400 )
     end
    
+end
+
+local function move( event )
+    if canMove then
+        canMove = false
+        event.target:setLabel("hola")
+        transition.to( group, { time=500, x=-_W} )
+    end
+end
+local function inputListener( event )
+    if event.phase == "began" then
+
+        -- user begins editing textBox
+        print( event.text )
+
+    elseif event.phase == "ended" then
+
+        -- do something with textBox's text
+    end
 end
 
 local function addLayer_dependence(sceneGroup)
@@ -39,10 +62,46 @@ local function addLayer_dependence(sceneGroup)
     local word_id = display.newText( "Búsqueda", _W*0.1, _H*0.5, "Quicksand-Bold", 17)
     word_id.anchorX = 0
     word_id:setFillColor( 1, 1,1 )
-   
+
+    list = widget.newButton
+    {
+        id = "agendaButton",
+        label = "Selecciona...",
+        onRelease = move,
+        emboss = false,
+        shape="rect",
+        width =_W*0.4,
+        height = _H*0.08,
+        fillColor = { default={ 0.85882353,0.85882353,0.85882353, 0.5 }, over={ 0.85882353,0.85882353,0.85882353, 0.5 } },
+        strokeColor = { default={ 0.7019, 0.101, 0.3450, 1 }, over={ 0.8, 0.8, 1, 1 } },
+        strokeWidth = 2.5
+    }
+    list.x = product_id.x+offset
+    list.anchorX = 0
+    list.y = product_id.y
+
+    list2 = widget.newButton
+    {
+        id = "agendaButton",
+        label = "Selecciona...",
+        onRelease = move,
+        emboss = false,
+        shape="rect",
+        width =_W*0.4,
+        height = _H*0.08,
+        fillColor = { default={ 0.85882353,0.85882353,0.85882353, 0.5 }, over={ 0.85882353,0.85882353,0.85882353, 0.5 } },
+        strokeColor = { default={ 0.7019, 0.101, 0.3450, 1 }, over={ 0.8, 0.8, 1, 1 } },
+        strokeWidth = 2.5
+    }
+    list2.x = word_id.x+(offset*1.5)
+    list2.anchorX = 0
+    list2.y = word_id.y
+
+
   
     sceneGroup:insert(titleTxt)
-
+    sceneGroup:insert(list)
+    sceneGroup:insert(list2)
     sceneGroup:insert(product_id)
     sceneGroup:insert(word_id)
 end
@@ -66,10 +125,68 @@ local function addLayer_advance(sceneGroup)
     word_id:setFillColor( 1, 1,1 )
 
 
+     list = widget.newButton
+    {
+        id = "agendaButton",
+        label = "Selecciona...",
+        onRelease = move,
+        emboss = false,
+        shape="rect",
+        width =_W*0.4,
+        height = _H*0.08,
+        fillColor = { default={ 0.85882353,0.85882353,0.85882353, 0.5 }, over={ 0.85882353,0.85882353,0.85882353, 0.5 } },
+        strokeColor = { default={ 0.7019, 0.101, 0.3450, 1 }, over={ 0.8, 0.8, 1, 1 } },
+        strokeWidth = 2.5
+    }
+    list.x = dependence_id.x+(offset*2)
+    list.anchorX = 0
+    list.y = dependence_id.y
+
+    list2 = widget.newButton
+    {
+        id = "agendaButton",
+        label = "Selecciona...",
+        onRelease = move,
+        emboss = false,
+        shape="rect",
+        width =_W*0.4,
+        height = _H*0.08,
+        fillColor = { default={ 0.85882353,0.85882353,0.85882353, 0.5 }, over={ 0.85882353,0.85882353,0.85882353, 0.5 } },
+        strokeColor = { default={ 0.7019, 0.101, 0.3450, 1 }, over={ 0.8, 0.8, 1, 1 } },
+        strokeWidth = 2.5
+    }
+    list2.x = product_id.x+(offset*1.5)
+    list2.anchorX = 0
+    list2.y = product_id.y
+
+
+    list3 = widget.newButton
+    {
+        id = "agendaButton",
+        label = "Selecciona...",
+        onRelease = move,
+        emboss = false,
+        shape="rect",
+        width =_W*0.4,
+        height = _H*0.08,
+        fillColor = { default={ 0.85882353,0.85882353,0.85882353, 0.5 }, over={ 0.85882353,0.85882353,0.85882353, 0.5 } },
+        strokeColor = { default={ 0.7019, 0.101, 0.3450, 1 }, over={ 0.8, 0.8, 1, 1 } },
+        strokeWidth = 2.5
+    }
+    list3.x = word_id.x+(offset*1.5)
+    list3.anchorX = 0
+    list3.y = word_id.y
+
+
+
+
     sceneGroup:insert(titleTxt)
     sceneGroup:insert(dependence_id)
     sceneGroup:insert(product_id)
     sceneGroup:insert(word_id)
+    sceneGroup:insert(list)
+    sceneGroup:insert(list2)
+    sceneGroup:insert(list3)
 end
 
 local function addLayer_product(sceneGroup)
@@ -80,9 +197,194 @@ local function addLayer_product(sceneGroup)
     local dependence_id = display.newText( "Lista de Productos", _W*0.1, _H*0.34,"Quicksand-Bold", 17)
     dependence_id.anchorX = 0
     dependence_id:setFillColor( 1, 1,1 )
+
+
+     list = widget.newButton
+    {
+        id = "agendaButton",
+        label = "Selecciona...",
+        onRelease = move,
+        emboss = false,
+        shape="rect",
+        width =_W*0.4,
+        height = _H*0.08,
+        fillColor = { default={ 0.85882353,0.85882353,0.85882353, 0.5 }, over={ 0.85882353,0.85882353,0.85882353, 0.5 } },
+        strokeColor = { default={ 0.7019, 0.101, 0.3450, 1 }, over={ 0.8, 0.8, 1, 1 } },
+        strokeWidth = 2.5
+    }
+    list.x = _W*0.5
+    
+    list.y = dependence_id.y + offset
+
    
     sceneGroup:insert(titleTxt)
     sceneGroup:insert(dependence_id)
+    sceneGroup:insert(list)
+end
+
+local function tableView(sceneGroup,table)
+
+    local keys = {
+        [1] = "TITULO",
+        [2] = "PRODUCTO"
+    }
+
+    search = string.upper(search)
+    local new_table = {}
+    local arrayNMX = db.find_by("NMX", keys, search, nil, 5)
+    local arrayNOM = db.find_by("NOM", keys, search, nil, 5)
+
+    if #arrayNMX > 0 then
+        for i=1, #arrayNMX do
+            new_table[#new_table+1] = arrayNMX[i]
+        end
+    end
+
+    if #arrayNOM > 0 then
+        for i=1, #arrayNMX do
+            new_table[#new_table+1] = arrayNMX[i]
+        end
+    end
+
+    if #new_table > 0 then
+        table = new_table
+    else
+        -- table = {
+        --     [1] = {
+        --         "TITULO" = "No hay resultados"
+        --     }
+        -- }
+        table[1] = {
+            TITULO = "No hay resultados"
+        }
+        print(#table)
+        
+    end
+
+    local tableViewColors = {
+        rowColor = { default = { 1 }, over = { 30/255, 144/255, 1 } },
+        lineColor = { 220/255 },
+        catColor = { default = { 150/255, 160/255, 180/255, 200/255 }, over = { 150/255, 160/255, 180/255, 200/255 } },
+        defaultLabelColor = { 0, 0, 0, 0.6 },
+        catLabelColor = { 0 }
+    }
+    tableViewColors.rowColor.default = { 48/255 }
+    tableViewColors.rowColor.over = { 72/255 }
+    tableViewColors.lineColor = { 36/255 }
+    tableViewColors.catColor.default = { 80/255, 80/255, 80/255, 0.9 }
+    tableViewColors.catColor.over = { 80/255, 80/255, 80/255, 0.9 }
+    tableViewColors.defaultLabelColor = { 1, 1, 1, 0.6 }
+    tableViewColors.catLabelColor = { 1 }
+
+    local tableView
+
+    -- Listen for tableView events
+    local function tableViewListener( event )
+        local phase = event.phase
+       
+        if event.phase == "ended" and table[event.target.index] ~= nil then
+            print( "Event.phase is:", table[event.target.index] )
+        else
+            composer.hideOverlay( "fade", 0 )
+        end
+    end
+    -- Handle row rendering
+    local function onRowRender( event )
+        local phase = event.phase
+        local row = event.row
+
+        local groupContentHeight = row.contentHeight
+        
+        if (#table > 0) then
+            print(#table)
+            local rowTitle = display.newText( row, table[row.index]['TITULO'], 0, 0, nil, 14 )
+            rowTitle.x = 10
+            rowTitle.anchorX = 0
+            rowTitle.y = groupContentHeight * 0.5       
+            rowTitle:setFillColor( unpack(row.params.defaultLabelColor) )  
+        end     
+    end
+    
+    -- Handle row updates
+    local function onRowUpdate( event )
+        local phase = event.phase
+        local row = event.row
+        --print( row.index, ": is now onscreen" )
+    end
+    
+    -- Handle touches on the row
+    local function onRowTouch( event )
+        local phase = event.phase
+        local row = event.target
+        if ( "release" == phase ) then
+        end
+    end
+    -- Create a tableView
+    tableView = widget.newTableView
+    {
+        top = actionbar.getHeight(),
+        left = -ox,
+        width = display.contentWidth+ox+ox, 
+        height = display.contentHeight,--+oy+oy-32,
+        hideBackground = true,
+        listener = tableViewListener,
+        onRowRender = onRowRender,
+        onRowUpdate = onRowUpdate,
+        onRowTouch = onRowTouch,
+    }
+    sceneGroup:insert( tableView )
+
+    local keys = {
+        [1] = "TITULO",
+        [2] = "PRODUCTO"
+    }
+
+    local arrayNMX = db.find_by("NMX", keys, search)
+    local arrayNOM = db.find_by("NOM", keys, search)
+
+    -- tableView:insertRow
+    -- {
+    --     isCategory = true,
+    --     rowHeight = rowHeight,
+    --     rowColor = rowColor,
+    --     lineColor = tableViewColors.lineColor,
+    --     params = v
+    -- }
+    -- for k,v in pairs(array) do
+    --     local isCategory = false
+    --     local rowHeight = 32
+    --     local rowColor = { 
+    --         default = tableViewColors.rowColor.default,
+    --         over = tableViewColors.rowColor.over,
+    --     }
+    --     tableView:insertRow
+    --     {
+    --         isCategory = isCategory,
+    --         rowHeight = rowHeight,
+    --         rowColor = rowColor,
+    --         lineColor = tableViewColors.lineColor,
+    --         params = { defaultLabelColor=tableViewColors.defaultLabelColor, catLabelColor=tableViewColors.catLabelColor }
+    --     }
+    -- end
+
+    -- Create 75 rows
+    for i = 1,#table do
+        local isCategory = false
+        local rowHeight = 32
+        local rowColor = { 
+            default = tableViewColors.rowColor.default,
+            over = tableViewColors.rowColor.over,
+        }
+        -- Insert the row into the tableView
+        tableView:insertRow
+        {
+            isCategory = isCategory,
+            rowHeight = rowHeight,
+            rowColor = rowColor,
+            lineColor = tableViewColors.lineColor,
+            params = { defaultLabelColor=tableViewColors.defaultLabelColor, catLabelColor=tableViewColors.catLabelColor }
+        }
+    end
 end
 --------------------------------End Functions-------------------------------------------------------------------
 
@@ -100,6 +402,7 @@ function scene:show( event )
     if phase == "will" then 
         print("show will ---search") 
         canTouch = true
+        canMove = true
         up = display.newRect( _W*0.5, _H*0.08, _W, _H*0.25)  
         up:setFillColor(0,0,0)
         up.alpha = 0.01
@@ -130,20 +433,24 @@ function scene:show( event )
          searchButton.x = _W*0.5
          searchButton.y = _H*0.7 
 
-        sceneGroup:insert(up)
-        sceneGroup:insert(down)
-        sceneGroup:insert(back)
-        sceneGroup:insert(background)
-        sceneGroup:insert(searchButton)
+        group:insert(up)
+        group:insert(down)
+        group:insert(back)
+        group:insert(background)
+        group:insert(searchButton)
+
+      
 
         if search_type == "advance" then   
-            addLayer_advance(sceneGroup)
+            addLayer_advance(group)
+            --tableView(group,table)
         elseif search_type == "dependence" then
-             addLayer_dependence(sceneGroup)
+             addLayer_dependence(group)
         elseif search_type == "product" then
-            addLayer_product(sceneGroup)
+            addLayer_product(group)
         end
-         slide_menu.move = false
+        slide_menu.move = false
+        sceneGroup:insert(group)
           
     elseif phase == "did" then
         print("show did ---search")    
---
+++ scripts/scenes/search_table.lua
@@ -0,0 +1,298 @@
+local composer = require( "composer" )
+local scene = composer.newScene()
+local widget = require( "widget" )
+local actionbar = require( "scripts.scenes.actionbar")
+local db = require( "scripts.libs.db")
+
+local _W = display.contentWidth
+local _H = display.contentHeight
+local halfW = display.contentCenterX
+local halfH = display.contentCenterY
+local ox, oy = math.abs(display.screenOriginX), math.abs(display.screenOriginY)
+
+local table = {}
+-- table[1] = {}
+
+local search = nil
+
+---------------------------------------------------------------------------------
+local function onKeyEvent( event )
+    -- Print which key was pressed down/up to the log.
+    local message = "Key '" .. event.keyName .. "' was pressed " .. event.phase
+    --print( message )
+    -- If the "back" key was pressed on Android, then prevent it from backing out of your app.
+    if (event.keyName == "back") and (system.getInfo("platformName") == "Android") and (event.phase == "up") then                           
+        composer.hideOverlay( "fade", 0 )
+    else
+        return false    
+    end
+end
+
+------------Functions.-----------------------------------------------------------------------
+local function Date( ... )
+    local widget = require( "widget" )
+
+    -- Create two tables to hold data for days and years      
+    local days = {}
+    local years = {}
+
+    -- Populate the "days" table
+    for d = 1, 31 do
+        days[d] = d
+    end
+
+    -- Populate the "years" table
+    for y = 1, 48 do
+        years[y] = 1969 + y
+    end
+
+    -- Configure the picker wheel columns
+    local columnData = 
+    {
+            -- Months
+        { 
+            align = "right",
+            width = 140,
+            startIndex = 5,
+            labels = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" }
+        },
+        -- Days
+        {
+            align = "center",
+            width = 60,
+            startIndex = 18,
+            labels = days
+        },
+        -- Years
+        {
+            align = "center",
+            width = 80,
+            startIndex = 10,
+            labels = years
+        }
+    }
+
+    -- Create the widget
+    local pickerWheel = widget.newPickerWheel
+    {
+    top = display.contentHeight - 222,
+    columns = columnData
+    }
+
+    -- Get the table of current values for all columns
+    -- This can be performed on a button tap, timer execution, or other event
+    local values = pickerWheel:getValues()
+
+    -- Get the value for each column in the wheel (by column index)
+    local currentMonth = values[1].value
+    local currentDay = values[2].value
+    local currentYear = values[3].value
+
+    print( currentMonth, currentDay, currentYear )
+end
+
+local function tableView(sceneGroup,table)
+
+    local keys = {
+        [1] = "TITULO",
+        [2] = "PRODUCTO"
+    }
+
+    search = string.upper(search)
+    local new_table = {}
+    local arrayNMX = db.find_by("NMX", keys, search, nil, 5)
+    local arrayNOM = db.find_by("NOM", keys, search, nil, 5)
+
+    if #arrayNMX > 0 then
+        for i=1, #arrayNMX do
+            new_table[#new_table+1] = arrayNMX[i]
+        end
+    end
+
+    if #arrayNOM > 0 then
+        for i=1, #arrayNMX do
+            new_table[#new_table+1] = arrayNMX[i]
+        end
+    end
+
+    if #new_table > 0 then
+        table = new_table
+    else
+        -- table = {
+        --     [1] = {
+        --         "TITULO" = "No hay resultados"
+        --     }
+        -- }
+        table[1] = {
+            TITULO = "No hay resultados"
+        }
+        print(#table)
+        
+    end
+
+    local tableViewColors = {
+        rowColor = { default = { 1 }, over = { 30/255, 144/255, 1 } },
+        lineColor = { 220/255 },
+        catColor = { default = { 150/255, 160/255, 180/255, 200/255 }, over = { 150/255, 160/255, 180/255, 200/255 } },
+        defaultLabelColor = { 0, 0, 0, 0.6 },
+        catLabelColor = { 0 }
+    }
+    tableViewColors.rowColor.default = { 48/255 }
+    tableViewColors.rowColor.over = { 72/255 }
+    tableViewColors.lineColor = { 36/255 }
+    tableViewColors.catColor.default = { 80/255, 80/255, 80/255, 0.9 }
+    tableViewColors.catColor.over = { 80/255, 80/255, 80/255, 0.9 }
+    tableViewColors.defaultLabelColor = { 1, 1, 1, 0.6 }
+    tableViewColors.catLabelColor = { 1 }
+
+    local tableView
+
+    -- Listen for tableView events
+    local function tableViewListener( event )
+        local phase = event.phase
+       
+        if event.phase == "ended" and table[event.target.index] ~= nil then
+            print( "Event.phase is:", table[event.target.index] )
+        else
+            composer.hideOverlay( "fade", 0 )
+        end
+    end
+    -- Handle row rendering
+    local function onRowRender( event )
+        local phase = event.phase
+        local row = event.row
+
+        local groupContentHeight = row.contentHeight
+        
+        if (#table > 0) then
+            print(#table)
+            local rowTitle = display.newText( row, table[row.index]['TITULO'], 0, 0, nil, 14 )
+            rowTitle.x = 10
+            rowTitle.anchorX = 0
+            rowTitle.y = groupContentHeight * 0.5       
+            rowTitle:setFillColor( unpack(row.params.defaultLabelColor) )  
+        end     
+    end
+    
+    -- Handle row updates
+    local function onRowUpdate( event )
+        local phase = event.phase
+        local row = event.row
+        --print( row.index, ": is now onscreen" )
+    end
+    
+    -- Handle touches on the row
+    local function onRowTouch( event )
+        local phase = event.phase
+        local row = event.target
+        if ( "release" == phase ) then
+        end
+    end
+    -- Create a tableView
+    tableView = widget.newTableView
+    {
+        top = actionbar.getHeight(),
+        left = -ox,
+        width = display.contentWidth+ox+ox, 
+        height = display.contentHeight,--+oy+oy-32,
+        hideBackground = true,
+        listener = tableViewListener,
+        onRowRender = onRowRender,
+        onRowUpdate = onRowUpdate,
+        onRowTouch = onRowTouch,
+    }
+    sceneGroup:insert( tableView )
+
+    local keys = {
+        [1] = "TITULO",
+        [2] = "PRODUCTO"
+    }
+
+    local arrayNMX = db.find_by("NMX", keys, search)
+    local arrayNOM = db.find_by("NOM", keys, search)
+
+    -- tableView:insertRow
+    -- {
+    --     isCategory = true,
+    --     rowHeight = rowHeight,
+    --     rowColor = rowColor,
+    --     lineColor = tableViewColors.lineColor,
+    --     params = v
+    -- }
+    -- for k,v in pairs(array) do
+    --     local isCategory = false
+    --     local rowHeight = 32
+    --     local rowColor = { 
+    --         default = tableViewColors.rowColor.default,
+    --         over = tableViewColors.rowColor.over,
+    --     }
+    --     tableView:insertRow
+    --     {
+    --         isCategory = isCategory,
+    --         rowHeight = rowHeight,
+    --         rowColor = rowColor,
+    --         lineColor = tableViewColors.lineColor,
+    --         params = { defaultLabelColor=tableViewColors.defaultLabelColor, catLabelColor=tableViewColors.catLabelColor }
+    --     }
+    -- end
+
+    -- Create 75 rows
+    for i = 1,#table do
+        local isCategory = false
+        local rowHeight = 32
+        local rowColor = { 
+            default = tableViewColors.rowColor.default,
+            over = tableViewColors.rowColor.over,
+        }
+        -- Insert the row into the tableView
+        tableView:insertRow
+        {
+            isCategory = isCategory,
+            rowHeight = rowHeight,
+            rowColor = rowColor,
+            lineColor = tableViewColors.lineColor,
+            params = { defaultLabelColor=tableViewColors.defaultLabelColor, catLabelColor=tableViewColors.catLabelColor }
+        }
+    end
+end
+--------------------------------End Functions-------------------------------------------------------------------
+
+-----------------------------------Scenes Functions---------------------------------
+function scene:create( event )
+    local sceneGroup = self.view
+end
+
+function scene:show( event )
+    local sceneGroup = self.view
+    local phase = event.phase
+
+    if phase == "will" then
+        search = event.params['search']
+        tableView(sceneGroup,table, search)
+    elseif phase == "did" then
+    end 
+end
+
+function scene:hide( event )
+    local sceneGroup = self.view
+    local phase = event.phase
+    
+    if event.phase == "will" then
+      
+    elseif phase == "did" then
+    end 
+end
+
+
+function scene:destroy( event )
+    local sceneGroup = self.view
+end
+------------------------------End Funcitons--------------------------------------
+---------------------------------------------------------------------------------
+-- Listener setup
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+--------------------------------------------------------------------------------
+return scene
